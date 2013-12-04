c72d3afb9319794d0c74706d2397dc1475013ec5
fer512 <fernandogabrieltamburro@gmail.com>
2013-12-04 17:19:14 -0300

    sp turnos

--- ScriptDeMigracion/FuncionesUtiles.sql
+++ ScriptDeMigracion/FuncionesUtiles.sql
@@ -5,6 +5,16 @@ RETURNS TABLE
 AS
 RETURN 
 (
+--un turno puede ser tomado cuando..
+-- no esta relacionado con ningun turno y la fecha es mayor a hoy o...
+-- cuando esta cancelado por el Afiliado y la fecha es mayor a hoy
+	  select A.FechaHora_Turno 
+	  from Free_Running.Agenda A left join Free_Running.Turno T on (T.Fecha = A.FechaHora_Turno)
+	  where (T.Numero is null or 
+      exists (select * from Free_Running.Turno_Cancelado TC where TC.Turno_Numero = T.Numero and TC.Cancelado_Por = 'Afiliado'))
+      and getdate() < A.FechaHora_Turno 
+      and A.Medico = @medico
+     /* 
 	select a.FechaHora_Turno from Free_Running.Agenda a
 	where
 	a.FechaHora_Turno <> all (select t.Numero --Los turnos no cancelados
@@ -14,6 +24,7 @@ RETURN
 	GETDATE()< a.FechaHora_Turno
 	and
 	a.Medico=@medico
-	
+	*/
 )
 GO
+
